513dd38aefc19e3fde259b55ad83b12a6033ac54
Taufiq Dhanani <dhanani94@gmail.com>
2020-11-12 08:07:03 -0500

    makes copy of image before annotating

--- src/detectors/base_detector.py
+++ src/detectors/base_detector.py
@@ -10,14 +10,15 @@ class BaseDetector:
         pass
 
     def draw_boxes(self, image, df):
+        annotated_image = image.copy()
         for idx, box in df.iterrows():
             x_min, y_min, x_max, y_max = box['x1'], box['y1'], box['x2'], box['y2']
             color = self.colors[int(box['class_id'])]
-            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), color, 2)
+            cv2.rectangle(annotated_image, (x_min, y_min), (x_max, y_max), color, 2)
             txt_loc = (max(x_min + 2, 0), max(y_min + 2, 0))
             txt = box['label']
-            image = draw_boxed_text(image, txt, txt_loc, color)
-        return image
+            annotated_image = draw_boxed_text(annotated_image, txt, txt_loc, color)
+        return annotated_image
 
     def prediction(self, image):
         raise NotImplementedError
